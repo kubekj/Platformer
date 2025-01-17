a1e532325f1ea884299d27332d0bb1a90ce5238f
kubekj <kuba.jedraszka@gmail.com>
2025-01-17 15:51:34 +0100

    Added dotnet clean arch solution

---
+++ yuiop/core/src/Application/Users/Register/UserRegisteredDomainEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Users;
+using MediatR;
+
+namespace Application.Users.Register;
+
+internal sealed class UserRegisteredDomainEventHandler : INotificationHandler<UserRegisteredDomainEvent>
+{
+    public Task Handle(UserRegisteredDomainEvent notification, CancellationToken cancellationToken) =>
+        // TODO: Send an email verification link, etc.
+        Task.CompletedTask;
+}
